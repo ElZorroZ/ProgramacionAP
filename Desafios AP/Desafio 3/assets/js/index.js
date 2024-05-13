0745b4afeaeb1a7258259e5e0ff5d8117ef2e30f
ElZorro <thiago2007crackz@gmail.com>
2024-05-13 18:55:15 -0300

    Desafio 4

--- Desafios AP/Desafio 3/assets/js/index.js
+++ Desafios AP/Desafio 3/assets/js/index.js
@@ -1,14 +1,8 @@
 let NombreProducto='Computadora'
 const PrecioUnitario = 2000
-const Descuento = 10
 let CantidadDeseada = prompt(`Ingresa la cantidad que desea comprar del producto: ${NombreProducto}, el precio es: ${PrecioUnitario}`)
-if(CantidadDeseada>=10){
-    let res_aux = PrecioUnitario*CantidadDeseada
-    let res = res_aux/Descuento
-    alert(`Costo total de la compra es: ${res_aux}\nCosto total con descuento es: ${res}`)
-
-    }
-else{
+function calcular_costo(PrecioUnitario,CantidadDeseada){
     let res = PrecioUnitario*CantidadDeseada
-    alert(`Costo total de la compra es: ${res}`)
-    }    
+    console.log(`Costo total de la compra es: ${res}`)
+    }
+
