90d53e1d301ca3fe361ed7160174f4e14ba558c1
ElZorro <thiago2007crackz@gmail.com>
2024-06-05 19:20:09 -0300

    Desafio 6

--- Desafios AP/Desafio 1/assets/js/index.js
+++
@@ -1,5 +0,0 @@
-let NombreProducto='Computadora'
-const PrecioUnitario = 2000
-let CantidadDeseada = prompt(`Ingresa la cantidad que desea comprar del producto: ${NombreProducto}, el precio es: ${PrecioUnitario}`)
-let res = PrecioUnitario*CantidadDeseada
-alert(`El costo total de la compra es: ${res}`)
--- Desafios AP/Desafio 3/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-let NombreProducto='Computadora'
-const PrecioUnitario = 2000
-let CantidadDeseada = prompt(`Ingresa la cantidad que desea comprar del producto: ${NombreProducto}, el precio es: ${PrecioUnitario}`)
-function calcular_costo(PrecioUnitario,CantidadDeseada){
-    let res = PrecioUnitario*CantidadDeseada
-    console.log(`Costo total de la compra es: ${res}`)
-    }
-
